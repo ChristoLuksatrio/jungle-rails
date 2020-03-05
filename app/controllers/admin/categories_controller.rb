9710930b3e78ea3e9e3485547b97acd22b00c0a4
Chris Luksatrio <ranggaluksatrio@gmail.com>
2020-03-05 16:27:35 +0000

    admin category page add

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,5 +10,22 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :description
+    )
+  end
+
 end
