d2c145cedaaad9ce661f4094e9410bcf4745f10b
Chris Luksatrio <ranggaluksatrio@gmail.com>
2020-03-05 18:55:52 +0000

    add user and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
 
+  # Login Functionality Routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
   resources :about, only: [:index]
 
   resources :products, only: [:index, :show]
