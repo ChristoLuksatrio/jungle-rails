0efcc53bd000ef2c9f158619db1cac5a43afc75f
Chris Luksatrio <ranggaluksatrio@gmail.com>
2020-03-09 20:55:34 +0000

    pass authentication add

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,15 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(email)
+      user
+    else
+      nil
+    end
+  end
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 3 }
